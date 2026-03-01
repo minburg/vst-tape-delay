3db5ec12db1e952217b77149d9a8967cbccc1f2a
Marinus <marinus.burger@gmail.com>
2026-03-01 11:36:09 +0100

    support using default audio interface for developing

--- LOCAL_DEVELOPMENT.md
+++ LOCAL_DEVELOPMENT.md
@@ -5,3 +5,5 @@ to get the vst3 plugin under target/bundled
 run
 cargo run --release
 for quick local testing
+
+cargo run --release -- --sample-rate 44100 --period-size 1024
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,16 @@ use nih_plug::prelude::*;
 use tape_delay::TapeDelay;
 
 fn main() {
-    nih_export_standalone::<TapeDelay>();
+    let mut args: Vec<String> = std::env::args().collect();
+
+    // If no arguments were provided (other than the executable name),
+    // inject defaults to prevent WASAPI initialization errors on standard interfaces.
+    if args.len() == 1 {
+        args.push("--sample-rate".to_string());
+        args.push("44100".to_string());
+        args.push("--period-size".to_string());
+        args.push("1024".to_string());
+    }
+
+    nih_export_standalone_with_args::<TapeDelay, _>(args.into_iter());
 }
